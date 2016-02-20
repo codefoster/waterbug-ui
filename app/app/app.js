3c046cf361557e1c57992c1b8455fcb47a25164d
Jeremy Foster <jeremy.foster@live.com>
2016-02-20 09:39:49 -0800

    pull out routing

--- app/app/app.js
+++ app/app/app.js
@@ -1,4 +1,4 @@
-System.register(['angular2/platform/browser', "angular2/core", "angular2/router", "../rower/rower", "../app.service/app.service", "../socket.service/socket.service", "angular2/http"], function(exports_1) {
+System.register(['angular2/platform/browser', "angular2/core", "../rower/rower", "../app.service/app.service", "../socket.service/socket.service", "angular2/http"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,7 +8,7 @@ System.register(['angular2/platform/browser', "angular2/core", "angular2/router"
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var browser_1, core_1, router_1, rower_1, app_service_1, socket_service_1, http_1;
+    var browser_1, core_1, rower_1, app_service_1, socket_service_1, http_1;
     var App;
     return {
         setters:[
@@ -17,9 +17,6 @@ System.register(['angular2/platform/browser', "angular2/core", "angular2/router"
             },
             function (core_1_1) {
                 core_1 = core_1_1;
-            },
-            function (router_1_1) {
-                router_1 = router_1_1;
             },
             function (rower_1_1) {
                 rower_1 = rower_1_1;
@@ -49,17 +46,16 @@ System.register(['angular2/platform/browser', "angular2/core", "angular2/router"
                     core_1.Component({
                         selector: "app",
                         templateUrl: 'app/app/app.html',
-                        directives: [router_1.ROUTER_DIRECTIVES, rower_1.Rower],
+                        directives: [rower_1.Rower],
                         providers: [http_1.HTTP_PROVIDERS],
                         styleUrls: ['app/app/app.css']
-                    }),
-                    router_1.RouteConfig([]), 
+                    }), 
                     __metadata('design:paramtypes', [socket_service_1.SocketService, app_service_1.AppService])
                 ], App);
                 return App;
             })();
             exports_1("App", App);
-            browser_1.bootstrap(App, [socket_service_1.SocketService, app_service_1.AppService, router_1.ROUTER_PROVIDERS, core_1.provide(router_1.LocationStrategy, { useClass: router_1.HashLocationStrategy })]);
+            browser_1.bootstrap(App, [socket_service_1.SocketService, app_service_1.AppService]);
         }
     }
 });
--- app/app/app.ts
+++ app/app/app.ts
@@ -1,14 +1,5 @@
 import {bootstrap} from 'angular2/platform/browser';
 import { Component, provide } from "angular2/core";
-import {
-    Router,
-    RouteConfig,
-    ROUTER_DIRECTIVES,
-    ROUTER_PROVIDERS,
-    LocationStrategy,
-    HashLocationStrategy
-} from "angular2/router";
-
 import { Rower } from "../rower/rower";
 import { AppService } from "../app.service/app.service"
 import { SocketService } from "../socket.service/socket.service"
@@ -17,17 +8,10 @@ import { HTTP_PROVIDERS } from "angular2/http";
 @Component({
     selector: "app",
     templateUrl: 'app/app/app.html',
-    directives: [ROUTER_DIRECTIVES, Rower],
+    directives: [Rower],
     providers: [HTTP_PROVIDERS],
     styleUrls: ['app/app/app.css']
 })
-
-// Configure the routes for the app
-// NOTE: this app doesn't actually even need a router or any of this routing code, but I'm leaving it in here for reference
-@RouteConfig([
-    // { path: "<path>", component: <component>, name: "<name>" }
-    // { path: "<path>", component: <component>, name: "<name>" }
-])
 
 export class App {
     constructor(private socketService: SocketService, private appService: AppService) {
@@ -40,11 +24,6 @@ export class App {
         // });
         
     }
-
-    //this is how to do conditional navigation (though I don't need to at this point)
-    // myfunction(router:Router) {
-    //     //router.navigate(["/"]);
-    // }
 }
 
-bootstrap(App, [SocketService, AppService, ROUTER_PROVIDERS, provide(LocationStrategy, { useClass: HashLocationStrategy })]);
+bootstrap(App, [SocketService, AppService]);
