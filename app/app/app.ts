59404d98ac30921a6ecc468c4234b55c2c6141ea
Jeremy Foster <jeremy.foster@live.com>
2016-04-04 15:13:23 -0700

    send 5 messages instead of 1 for now

--- app/app/app.ts
+++ app/app/app.ts
@@ -1,4 +1,4 @@
-import {bootstrap} from 'angular2/platform/browser';
+import { bootstrap } from 'angular2/platform/browser';
 import { Component, provide } from "angular2/core";
 import { Rower } from "../rower/rower";
 import { AppService } from "../app.service/app.service"
@@ -11,8 +11,8 @@ import { HTTP_PROVIDERS } from "angular2/http";
     providers: [HTTP_PROVIDERS],
     styleUrls: ['app/app/app.css']
 })
-
 export class App {
+    
     constructor(private appService: AppService) {
         // socket.emit('msg', 'hello from the client');
 
@@ -21,20 +21,23 @@ export class App {
         //NOTE: I don't like how I'm having to pass the appService to the other function here
         this.startRace();
 
-        //this emits a stroke to the server
-        //this would normally be done on each actual stroke of the machine        
-        socket.emit("stroke", {
-            name: 'jeremy',
-            strokeRate: 23, //note that this is only a _single_ stroke rate value
-            caloriesPerMinute: 78,
-            distance: 99
-        });
-        
+        //this emits 5 strokes to the server
+        //this would normally be done on each actual stroke of the machine
+        let distance = 0;
+        for (var i = 0; i < 5; i++) {
+            socket.emit("stroke", {
+                name: 'jeremy',
+                strokeRate: (Math.random() * 5) + 20,
+                caloriesPerMinute: (Math.random() * 10) + 70,
+                distance: distance += (Math.random()) 
+            });
+        }
+
         //handle a stroke message
         socket.on("stroke", function(data) {
             //update our app state with the new message
             //will require adding the stroke rate to the user's array
-            console.log(JSON.stringify(appService.state));
+            console.log(JSON.stringify(data));
         });
     }
 
