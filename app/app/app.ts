efc59c9a0751744823804a24db5b80bbd329725c
Jeremy Foster <jeremy.foster@live.com>
2016-04-06 09:55:14 -0700

    add start race and stop race buttons

--- app/app/app.ts
+++ app/app/app.ts
@@ -6,30 +6,48 @@ import { HTTP_PROVIDERS } from "angular2/http";
 
 @Component({
     selector: "app",
-    templateUrl: 'app/app/app.html',
+    templateUrl: "app/app/app.html",
     directives: [Rower],
     providers: [HTTP_PROVIDERS],
-    styleUrls: ['app/app/app.css']
+    styleUrls: ["app/app/app.css"]
 })
 export class App {
-    
-    constructor(private appService: AppService) {
-        var socket = io("http://cf.ngrok.io");
+    private socket;
 
-        //NOTE: I don't like how I'm having to pass the appService to the other function here
-        this.startRace();
+    constructor(private appService: AppService) {
+        this.socket = io("http://cf.ngrok.io");
 
         //handle a stroke message
-        socket.on("stroke", function(data) {
-            //update our app state with the new message
+        this.socket.on("stroke", (data) => {
+            //TODO: update our app state with the new message
             //will require adding the stroke rate to the user's array
             console.log(JSON.stringify(data));
         });
+
+        this.socket.on("startrace", (racedata) => {
+            this.appService.state.startTime = racedata.startTime;
+            this.appService.state.raceDistance = racedata.distance;
+            console.log(`race started\n  start time: ${this.appService.state.startTime}\n  race distance: ${this.appService.state.raceDistance}`);
+        })
+
+        this.socket.on("stoprace", () => {
+            // a non-null value for this.appService.state.startTime should mean a race is currently underway
+            // to stop a race, simply null that value
+            this.appService.state.startTime = null;
+            console.log("race stopped");
+        })
     }
 
     startRace() {
-        // appService.state.startTime = moment();
-        console.log('|' + this.appService.state.startTime + '|')
+        let racedata = {
+            startTime: new Date(),
+            distance: 500 //hard code to 500m for now
+        };
+        this.socket.emit("startrace", racedata);
+    }
+    
+    stopRace() {
+        this.socket.emit("stoprace");
     }
 }
 
