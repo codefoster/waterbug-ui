d2a7a753056896f70c4c6ac310da01d290d3819b
Jeremy Foster <jeremy.foster@live.com>
2016-04-08 16:43:04 -0700

    refactor race.component out of app.component so that app.component doesn't contain domain specific logic

--- app/app.component/app.component.ts
+++ app/app.component/app.component.ts
@@ -1,53 +1,14 @@
 import { Component, provide } from "angular2/core";
 import { AppService } from "../app.service/app.service"
-import { Rower } from "../rower.component/rower.component";
-import { HTTP_PROVIDERS } from "angular2/http";
+import { Race } from "../race.component/race.component";
 
 @Component({
     selector: "app",
     templateUrl: "app/app.component/app.component.html",
-    directives: [Rower],
-    providers: [HTTP_PROVIDERS],
-    styleUrls: ["app/app.component/app.component.css"]
+    directives: [Race],
 })
 export class AppComponent {
-    private socket;
-
-    constructor(private appService: AppService) {
-        this.socket = io("http://cf.ngrok.io");
-        
-        //handle a stroke message
-        this.socket.on("stroke", (data) => {
-            //TODO: update our app state with the new message
-            //will require adding the stroke rate to the user's array
-            
-            console.log(JSON.stringify(data));
-        });
-
-        this.socket.on("startrace", (racedata) => {
-            this.appService.state.startTime = racedata.startTime;
-            this.appService.state.raceDistance = racedata.distance;
-            console.log(`race started\n  start time: ${this.appService.state.startTime}\n  race distance: ${this.appService.state.raceDistance}`);
-        })
-
-        this.socket.on("stoprace", () => {
-            // a non-null value for this.appService.state.startTime should mean a race is currently underway
-            // to stop a race, simply null that value
-            this.appService.state.startTime = null;
-            console.log("race stopped");
-        })
-    }
-
-    startRace() {
-        let racedata = {
-            startTime: new Date(),
-            distance: 500 //hard code to 500m for now
-        };
-        this.socket.emit("startrace", racedata);
-    }
-    
-    stopRace() {
-        this.socket.emit("stoprace");
+    constructor() {
     }
 }
 
