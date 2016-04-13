c3b86bd05311f9762b433d00bedd79d759a236e2
Jeremy Foster <jeremy.foster@live.com>
2016-04-13 16:22:52 -0700

    add test framework and first (stub) test for race.service.spec

---
+++ app/race.service/race.service.spec.ts
@@ -0,0 +1,11 @@
+import { RaceService } from './race.service';
+
+describe('RaceService Tests', () => {
+
+    it('RaceService exists', () => {
+        expect(RaceService).not.toBeNull();
+    });
+    
+    
+
+});
