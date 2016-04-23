941069bf8b62f551e9e8f1a1bf4c0e92494a58dc
Jeremy Foster <jeremy.foster@live.com>
2016-04-23 01:47:48 -0700

    update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -101,6 +101,7 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
+  call :ExecuteCmd !NPM_CMD! install typescript
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
