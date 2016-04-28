59be12e05d57a99b8311ee79ede1292de012d247
Jeremy Foster <jeremy.foster@live.com>
2016-04-28 15:52:33 -0700

    attempt to bring SocketService in to AppComponent

--- app/app.component/app.component.ts
+++ app/app.component/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, provide } from "angular2/core";
-import { RaceComponent } from "../race.component/race.component";
+// import { RaceComponent } from "../race.component/race.component";
+import { SocketService } from "../socket.service/socket.service";
 
 @Component({
     selector: "app",
     templateUrl: "app/app.component/app.component.html",
-    directives: [RaceComponent],
+    // directives: [RaceComponent],
     styleUrls: ["app/app.component/app.component.css"],
 })
-export class AppComponent {}
+export class AppComponent {
+    constructor(private socketService:SocketService){
+        this.socketService.send('{"message":"hi"}');
+    }
+}
 
