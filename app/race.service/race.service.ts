249ed45714a0ba087bd9792e00d5ada8fbcf35a4
Jeremy Foster <jeremy.foster@live.com>
2016-04-28 14:25:26 -0700

    flatten sockets by adding message property to messages and using .send instead of .emit

--- app/race.service/race.service.ts
+++ app/race.service/race.service.ts
@@ -15,69 +15,72 @@ export class RaceService {
         // this.socket = io("http://server.waterbug.site");
         this.socket = io("http://localhost:8080");
 
-        //handle a stroke message
-        this.socket.on("stroke", (data) => {
-            console.log('onstroke');
-            //TODO: update our app state with the new message
-            //will require adding the stroke rate to the user's array
-            console.log(`stroke received from ${data.name}`);
-
-            // add user if not exist
-            if (!this.rowers.some(r => r.name == data.name)) {
-                console.log(`adding ${data.name}`);
-                this.rowers.push({
-                    name: data.name,
-                    strokeRates: [],
-                    caloriesPerMinute: 0,
-                    distance: 0
-                });
-            }
-
-            if (this.raceOn) {
-                let r = this.rowers.filter(r => r.name == data.name)[0];
-                r.strokeRates.push(data.strokeRate);
-                r.distance = Math.min(this.raceDistance, r.distance + data.distance);
-                if (r.distance >= this.raceDistance) {
-                    //TODO:declare winner
+        this.socket.on("message", d => {
+            switch(d.message) {
+                case "startrace":
+                    this.startTime = d.startTime;
+                    this.raceDistance = d.distance;
+                    this.rowers.forEach(r => { r.distance = 0; r.strokeRates = []; });
+                    console.log(`race started\n  start time: ${this.startTime}\n  race distance: ${this.raceDistance}`);
+                    break;
+                case "stoprace":
+                    // a non-null value for this.raceService.startTime should mean a race is currently underway
+                    // to stop a race, simply null that value
                     this.startTime = null;
-                }
+                    console.log("race stopped");
+                    break;
+                case "stroke": 
+                    //TODO: update our app state with the new message
+                    //will require adding the stroke rate to the user's array
+                    console.log(`stroke received from ${d.name}`);
+
+                    // add user if not exist
+                    if (!this.rowers.some(r => r.name == d.name)) {
+                        console.log(`adding ${d.name}`);
+                        this.rowers.push({
+                            name: d.name,
+                            strokeRates: [],
+                            caloriesPerMinute: 0,
+                            distance: 0
+                        });
+                    }
+
+                    if (this.raceOn) {
+                        let r = this.rowers.filter(r => r.name == d.name)[0];
+                        r.strokeRates.push(d.strokeRate);
+                        r.distance = Math.min(this.raceDistance, r.distance + d.distance);
+                        if (r.distance >= this.raceDistance) {
+                            //TODO:declare winner
+                            this.startTime = null;
+                        }
+                    }
+                    break;
             }
         });
-
-        this.socket.on("startrace", (racedata) => {
-            this.startTime = racedata.startTime;
-            this.raceDistance = racedata.distance;
-            this.rowers.forEach(r => { r.distance = 0; r.strokeRates = []; });
-            console.log(`race started\n  start time: ${this.startTime}\n  race distance: ${this.raceDistance}`);
-        })
-
-        this.socket.on("stoprace", () => {
-            // a non-null value for this.raceService.startTime should mean a race is currently underway
-            // to stop a race, simply null that value
-            this.startTime = null;
-            console.log("race stopped");
-        })
     }
 
     simulateStroke(name: string) {
-        this.socket.emit("stroke", {
+        this.socket.send({
+            message: "stroke",
             name: name,
             strokeRate: Math.round((Math.random() * 5) + 20),
             caloriesPerMinute: Math.round((Math.random() * 10) + 70),
             distance: Math.round((Math.random() * 5) + 20)
-        });
+        })
     }
 
     startRace() {
-        let racedata = {
+        this.socket.send({
+            message: "startrace",
             startTime: new Date(),
             distance: 500 //hard code to 500m for now
-        };
-        this.socket.emit("startrace", racedata);
+        });
     }
 
     stopRace() {
-        this.socket.emit("stoprace");
+        this.socket.send({
+            message: "stoprace"
+        });
     }
 
 }
