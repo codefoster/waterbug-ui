b6e3808df385f68e2eb0dd68c2db338ea4087b84
Jeremy Foster <jeremy.foster@live.com>
2016-05-04 12:34:41 -0700

    remove --sourceMap option from tsc command in deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -108,7 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 
 :: 4. Compile TypeScript
 echo Transpiling TypeScript in %DEPLOYMENT_TARGET%...
-call :ExecuteCmd tsc -p "%DEPLOYMENT_TARGET%" --sourceMap=false
+call :ExecuteCmd tsc -p "%DEPLOYMENT_TARGET%"
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 goto end
