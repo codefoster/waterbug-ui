655b70bd1d4292a13adccab04821e501e21e63e7
Jeremy Foster <jeremy.foster@live.com>
2016-05-04 00:13:41 -0700

    stabilize, remove socketservice, use npm for socket.io-client

--- app/app.component/app.component.ts
+++ app/app.component/app.component.ts
@@ -1,16 +1,14 @@
 import { Component, provide } from "angular2/core";
-// import { RaceComponent } from "../race.component/race.component";
-import { SocketService } from "../socket.service/socket.service";
+import { RaceComponent } from "../race.component/race.component";
 
 @Component({
     selector: "app",
     templateUrl: "app/app.component/app.component.html",
-    // directives: [RaceComponent],
+    directives: [RaceComponent],
     styleUrls: ["app/app.component/app.component.css"],
 })
 export class AppComponent {
-    constructor(private socketService:SocketService){
-        this.socketService.send('{"message":"hi"}');
+    constructor(){
     }
 }
 
