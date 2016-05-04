36db16b9e4e12508ac9023525ba073cf1ce4ddb4
Jeremy Foster <jeremy.foster@live.com>
2016-05-04 09:25:47 -0700

    finish renaming race-controls to dashboard

--- app/dashboard.component/dashboard.component.ts
+++ app/dashboard.component/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, provide } from "angular2/core";
 import { RaceService } from "../race.service/race.service"
 
 @Component({
-    selector: "race-controls",
+    selector: "dashboard",
     templateUrl: "app/dashboard.component/dashboard.component.html",
     styleUrls: ["app/dashboard.component/dashboard.component.css"]
 })
