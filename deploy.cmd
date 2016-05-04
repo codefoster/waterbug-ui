fd150dd748d14d79f2a025268ec1694758c9574e
Jeremy Foster <jeremy.foster@live.com>
2016-05-04 10:43:55 -0700

    Remove line to install typescript from deploy.com and move it to the depencies section of package.json

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,6 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install --production
-  call :ExecuteCmd !NPM_CMD! install typescript
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
