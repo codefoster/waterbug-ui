d0bc323ca0c6fd669797669fe93467577a9e8073
Jeremy Foster <jeremy.foster@live.com>
2016-05-10 23:24:30 -0700

    use Rx for socket messages, animate rower

--- app/race.service/race.service.ts
+++ app/race.service/race.service.ts
@@ -1,66 +1,79 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 import { Config } from '../config';
+import { Observable, Subject} from 'rxjs/Rx';
 
 @Injectable()
 export class RaceService {
-    private socket:SocketIOClient.Socket;
+    private socket: SocketIOClient.Socket;
     config: any = new Config();
     raceDistance: number = this.config.defaultRaceDistance;
     startTime: Date;
     rowers: any[] = [];
+
+    private messages = new Subject();
+    messages$: Observable<any> = this.messages.asObservable();
+    strokes$ = this.messages$.filter(m => m.message == "stroke");
 
     get raceOn(): boolean {
         return this.startTime != null;
     }
 
     constructor() {
+        //connect to the socket server
         this.socket = io.connect(this.config.socketServerUrl);
         
-        this.socket.on("message", d => {
-            switch(d.message) {
-                case "startrace":
-                    this.startTime = d.startTime;
-                    this.raceDistance = d.distance;
-                    this.rowers.forEach(r => { r.distance = 0; r.strokeRates = []; });
-                    console.log(`race started\n  start time: ${this.startTime}\n  race distance: ${this.raceDistance}`);
-                    break;
-                case "stoprace":
-                    // a non-null value for this.raceService.startTime should mean a race is currently underway
-                    // to stop a race, simply null that value
-                    this.startTime = null;
-                    console.log("race stopped");
-                    break;
-                case "stroke": 
-                    //TODO: update our app state with the new message
-                    //will require adding the stroke rate to the user's array
-                    console.log(`stroke received from ${d.name}`);
+        //stream all messages into our Rx Subject
+        this.socket.on("message", d => this.messages.next(d));
 
-                    // add user if not exist
-                    if (!this.rowers.some(r => r.name == d.name)) {
-                        console.log(`adding ${d.name}`);
-                        this.rowers.push({
-                            name: d.name,
-                            strokeRates: [],
-                            caloriesPerMinute: 0,
-                            distance: 0
-                        });
-                    }
+        //handle the strokes
+        this.strokes$.subscribe(d => {
+            //TODO: update our app state with the new message
+            //will require adding the stroke rate to the user's array
+            console.log(`stroke received from ${d.name}`);
 
-                    if (this.raceOn) {
-                        let r = this.rowers.filter(r => r.name == d.name)[0];
-                        r.strokeRates.push(d.strokeRate);
-                        r.distance = Math.min(this.raceDistance, r.distance + d.distance);
-                        if (r.distance >= this.raceDistance) {
-                            //TODO:declare winner
-                            this.startTime = null;
-                        }
-                    }
-                    break;
-                case "removerower":
-                    this.rowers = this.rowers.filter(r => r.name != d.name);
-                    break;
+            // add user if not exist
+            if (!this.rowers.some(r => r.name == d.name)) {
+                console.log(`adding ${d.name}`);
+                this.rowers.push({
+                    name: d.name,
+                    strokeRates: [],
+                    caloriesPerMinute: 0,
+                    distance: 0
+                });
             }
+
+            if (this.raceOn) {
+                let r = this.rowers.filter(r => r.name == d.name)[0];
+                r.strokeRates.push(d.strokeRate);
+                r.distance = Math.min(this.raceDistance, r.distance + d.distance);
+                if (r.distance >= this.raceDistance) {
+                    //TODO:declare winner
+                    this.startTime = null;
+                }
+            }
+        })
+
+        //handle race start events 
+        this.messages$.filter(m => m.message == "startrace").subscribe(d => {
+            this.startTime = d.startTime;
+            this.raceDistance = d.distance;
+            this.rowers.forEach(r => { r.distance = 0; r.strokeRates = []; });
+            console.log(`race started\n  start time: ${this.startTime}\n  race distance: ${this.raceDistance}`);
+        });
+
+        //handle race stop events        
+        this.messages$.filter(m => m.message == "stoprace").subscribe(d => {
+            // a non-null value for this.raceService.startTime should mean a race is currently underway
+            // to stop a race, simply null that value
+            this.startTime = null;
+            console.log("race stopped");
+        });
+
+        // handle remove rower events        
+        this.messages$.filter(m => m.message == "removerower").subscribe(d => {
+            this.rowers = this.rowers.filter(r => r.name != d.name);
+            console.log(`${d.name} removed`);
         });
     }
 
@@ -87,8 +100,8 @@ export class RaceService {
             message: "stoprace"
         });
     }
-    
-    removeRower(name:string) {
+
+    removeRower(name: string) {
         this.socket.send({
             message: "removerower",
             name: name
--- app/rower.component/rower.component.ts
+++ app/rower.component/rower.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { StrokeGraphComponent } from '../stroke-graph.component/stroke-graph.component';
 import { RaceService } from '../race.service/race.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
     selector: 'rower',
@@ -10,8 +11,19 @@ import { RaceService } from '../race.service/race.service';
 })
 export class RowerComponent {
     @Input() public rower: any;
-    
-    constructor(private raceService: RaceService) {
-    }
+    private rowingSequence:Observable<string>;
+    rowerImageUrl: string = '/app/rower.component/assets/rower-1.png';
 
+    constructor(private raceService: RaceService) {
+        this.raceService.strokes$.filter(s => s.name == this.rower.name).subscribe(d => {
+            if(this.rower.distance >= this.raceService.raceDistance) {
+                this.rowerImageUrl = `/app/rower.component/assets/rower-yay.png`;
+            }
+            else {
+                this.rowingSequence = Observable.interval(1000).take(5).map(n => `/app/rower.component/assets/rower-${n}.png`);
+                this.rowingSequence.subscribe(value => this.rowerImageUrl = value);
+            }                
+            
+        })
+    }
 }
